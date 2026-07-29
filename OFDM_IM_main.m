63a025fa329381f3e6217cad88baa90b587238c3
Carlos Henrique Vendramin <carloshenriquevendramin@gmail.com>
2026-07-29 10:47:20 -0300

    Adicionado sensoriamento com mapa range doppler

--- OFDM_IM_main.m
+++ OFDM_IM_main.m
@@ -3,9 +3,9 @@ clear all; clc; close all;
 N = 16;              % total de subportadoras
 K = 2;              % Número de subportadoras ativas
 M = 16;              % Ordem da modulação (M-QAM)
-num_blocos = 1000;  % Numero de amostras a serem transmitidas no loop
+num_blocos = 10000;  % Numero de amostras a serem transmitidas no loop
 %nFFT = 124; %Numero de FFT'S
-nCicP = 8;
+nCicP = 9;
 
 
 EbNo_vec = 0:2:20;
@@ -87,14 +87,51 @@ for snr_indice = 1:length(EbNo_vec)
     txstate = zeros(max(length(a_filt)-1, length(b_filt)-1), 1);
     rxstate = zeros(max(length(a_filt)-1, length(b_filt)-1), 1);
     
+
+%--------------------------------------------------------%
+%               variaveis sensoriamento                  %
+%--------------------------------------------------------%
+
+distancia=1;
+velocidade=1;
+
+Radar_tx_Matriz = zeros(N, num_blocos);
+Radar_rx_Matriz = zeros(N, num_blocos);
+
+
+
+
+%----------------------------------------------%
+
     for b = 1:num_blocos
         
         [x_bloco, y_bloco, bits_tx, txstate, rxstate, tempo] = TxRx_Cascata(fs, Fator_Potencia, p, SNR_dB, LUT, N, nCicP, p1, M, K, a_filt, b_filt, txstate, rxstate, tempo, cfo_hz, Delta_tx, Delta_rx);
         
         [bits_rx, erros, x_total, y_total, y_eq] = eqlt(b, N, x_bloco, y_bloco, H_eff, Catalogo_X, Catalogo_Bits,bits_tx, erros, x_total, y_total, y_eq);
             
+    
+
+    %-----------------------------------------%
+    %              Sensoriamento              %
+    %-----------------------------------------%
+    
+        fase_range=exp(-1i * 2 * pi * (0:N-1).' * distancia / N);
+        fase_doppler= exp(1i * 2 * pi * velocidade * b / num_blocos);
+    
+        y_radar_bloco = (x_bloco .* fase_range) * fase_doppler;
+        y_radar_bloco = awgn(y_radar_bloco, SNR_dB, 'measured');
+        
+        Radar_tx_Matriz(:, b) = x_bloco;
+        Radar_rx_Matriz(:, b) = y_radar_bloco;
+    %-----------------------------------------%
+
     end
     
+    filtro_casado = Radar_rx_Matriz .* conj(Radar_tx_Matriz);
+    Range = ifft(filtro_casado, N, 1);
+    Doppler = fftshift(fft(Range, num_blocos, 2), 2);
+
+
     %-----------------------------------------%
     %      Iniciando a calculo da BER         %
     %-----------------------------------------%
@@ -134,7 +171,7 @@ png_constelacao = ['constelacao_ofdm_im_HardwareLimitado_', timestamp, '.png'];
 exportgraphics(fig_constelacao, png_constelacao, 'Resolution', 300);
 
 
-%% 4. PLOTAR A CURVA DE BER
+% 4. Curva BER
 fig_BER=figure;
 % semilogy é usado porque a BER decai exponencialmente (eixo Y logarítmico)
 semilogy(EbNo_vec, BER, 'bo-', 'LineWidth', 2, 'MarkerSize', 8, 'MarkerFaceColor', 'b');
@@ -147,3 +184,17 @@ png_BER = ['BER_ofdm_im_HardwareLimitado_', timestamp, '.png'];
 exportgraphics(fig_BER, png_BER, 'Resolution', 300);
 
 %-----------------------------------------------------------------------------
+
+% 5. Mapa range doppler
+fig_RangeDoppler=figure;
+doppler_axis = linspace(0, 10, num_blocos);
+range_axis = 0:(N-1);
+
+mesh(doppler_axis, range_axis, 10*log10(abs(Range).^2));
+view(2);
+xlabel('Frequência Doppler (Velocidade)');
+ylabel('Range (Distância)');
+title(['Mapa Range-Doppler OFDM-IM | SNR = ', num2str(SNR_dB), ' dB']);
+colorbar;
+png_RD = ['RangeDoppler_ofdm_im_HardwareLimitado_', timestamp, '.png'];
+exportgraphics(fig_RangeDoppler, png_RD, 'Resolution', 300);
