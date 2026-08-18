7849dccd4058f2c4e1a28ed671b4b7acf53420a5
Carlos Henrique Vendramin <carloshenriquevendramin@gmail.com>
2026-08-18 13:48:14 -0300

    Correções de código e melhorias

--- eqlt.m
+++ eqlt.m
@@ -1,7 +1,7 @@
 %-----------------------------------------%
 %               Equalizaçao               %
 %-----------------------------------------%
-function [bits_rx, erros, x_total, y_total, y_eq] = eqlt(b, N, x_bloco, y_bloco, H_eff, Catalogo_X, Catalogo_Bits, bits_tx, erros, x_total, y_total, y_eq)
+function [bits_rx, erros, x_total, y_total, y_eq] = eqlt(b, N, n, G, p_grupo, x_bloco, y_bloco, H_eff, Catalogo_X, Catalogo_Bits, bits_tx, erros, x_total, y_total, y_eq)
 
     VetorBeg = (b-1) * N + 1;
     VetorEnd = b * N;
@@ -10,15 +10,23 @@ function [bits_rx, erros, x_total, y_total, y_eq] = eqlt(b, N, x_bloco, y_bloco,
     y_total(VetorBeg:VetorEnd) = y_bloco;
     y_eq(VetorBeg:VetorEnd) = y_bloco ./ H_eff; % Apenas para visualização sem anéis
 
-    Catalogo_X_filt = Catalogo_X .* H_eff;
-    distancias = sum(abs(y_bloco - Catalogo_X_filt).^2, 1);
+    bits_rx = zeros(1, p_grupo * G);
+    
+    for g = 1:G
+        idx_start = (g-1)*n + 1;
+        idx_end = g*n;
+        
+        y_grupo = y_bloco(idx_start:idx_end);
+        H_eff_grupo = H_eff(idx_start:idx_end);
+        
+        Catalogo_X_filt = Catalogo_X .* H_eff_grupo;
+        distancias = sum(abs(y_grupo - Catalogo_X_filt).^2, 1);
 
-    %encontrando o indice de menor distancia
-    [~, indice_menor] = min(distancias);
-    bits_rx = Catalogo_Bits(indice_menor, :);
+        %encontrando o indice de menor distancia
+        [~, indice_menor] = min(distancias);
+        bits_rx((g-1)*p_grupo + 1 : g*p_grupo) = Catalogo_Bits(indice_menor, :);
+    end
 
     erros_bloco = sum(bits_tx ~= bits_rx);
-    
     erros = erros + erros_bloco;
-
 end
--- reconstrutor.m
+++ reconstrutor.m
@@ -1,20 +1,32 @@
 %--------------------------------------------%
 %                  Receptor                  %
 %--------------------------------------------%
-function [rxstate, y_bloco, y_Cic_Final] = reconstrutor(cfo_hz, N, Vetor_tempo, y_Cic, a_filt, b_filt, Delta_rx, nCicP, rxstate)
+function [rxstate, y_bloco, y_Cic_Final] = reconstrutor(cfo_hz, N, Vetor_tempo, y_Cic, a_filt, b_filt, Delta_rx, nCicP, rxstate, Compensa_CFO, Habilita_Quantizacao, Habilita_Filtro)
 
     %compensando o CFO antes da filtragem evita rotaçao de fase por atraso de grupo
-    Cfo_compensacao = exp(-1i * 2 * pi * cfo_hz * Vetor_tempo);
-    y_Cic_Compensado = y_Cic .* Cfo_compensacao;
-
+    if Compensa_CFO
+        Cfo_compensacao = exp(-1i * 2 * pi * cfo_hz * Vetor_tempo);
+        y_Cic_Compensado = y_Cic .* Cfo_compensacao;
+    else
+        y_Cic_Compensado = y_Cic;
+    end
+    
     %--------------------------------------------%
     %              Reconstruçao                  %
     %--------------------------------------------%
-    [y_Cic_Filtrado, rxstate] = filter(b_filt, a_filt, y_Cic_Compensado, rxstate);
-
-    rx_re = round(real(y_Cic_Filtrado)/Delta_rx)*Delta_rx;
-    rx_im = round(imag(y_Cic_Filtrado)/Delta_rx)*Delta_rx;
-    y_Cic_Final = rx_re + (1i*rx_im);
+   if Habilita_Filtro
+        [y_Cic_Filtrado, rxstate] = filter(b_filt, a_filt, y_Cic_Compensado, rxstate);
+    else
+        y_Cic_Filtrado = y_Cic_Compensado;
+    end
+    
+    if Habilita_Quantizacao
+        rx_re = round(real(y_Cic_Filtrado)/Delta_rx)*Delta_rx;
+        rx_im = round(imag(y_Cic_Filtrado)/Delta_rx)*Delta_rx;
+        y_Cic_Final = rx_re + (1i*rx_im);
+    else
+        y_Cic_Final = y_Cic_Filtrado;
+    end
 
     y_tempo = y_Cic_Final(nCicP+1:end);
     y_bloco = fft(y_tempo, N);
