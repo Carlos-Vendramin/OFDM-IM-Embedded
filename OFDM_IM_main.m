c71983398cfc1d066e321aac8e69cd02405a4102
Carlos Henrique Vendramin <carloshenriquevendramin@gmail.com>
2026-08-18 13:48:14 -0300

    Correções de código e melhorias

--- OFDM_IM_main.m
+++ OFDM_IM_main.m
@@ -1,31 +1,29 @@
 clear all; clc; close all;
 %% Configuraçao do transmissor OFDM-IM
-N = 16;              % total de subportadoras
-K = 2;              % Número de subportadoras ativas
+N = 64;              % total de subportadoras
+G = 16;               
+n = N / G;           
+k = 3;               
 M = 16;              % Ordem da modulação (M-QAM)
-num_blocos = 10000;  % Numero de amostras a serem transmitidas no loop
+num_blocos = 1000;  % Numero de amostras a serem transmitidas no loop
 %nFFT = 124; %Numero de FFT'S
 nCicP = 9;
 
-
-EbNo_vec = 0:2:20;
+EbNo_vec = 0:1:20;
 BER = zeros(1, length(EbNo_vec));
 
 %----------------------------------%
 %       Correçao da potencia       %
 %----------------------------------%
-
-Pot_N=N/K;
-Fator_Potencia=sqrt(Pot_N);
-
+Pot_N = n/k;
+Fator_Potencia = sqrt(Pot_N);
 
 %-------------------------------------------%
 %       Frequencia e largura de banda       %
 %-------------------------------------------%
-[Fc, Bw, fs, F_Corte, a_filt,b_filt, H_freq, w, H_eff] =freq_bw(N,2.4e9, 20e6, 20e6, 9.9e6);
-%Correto
+[Fc, Bw, fs, F_Corte, a_filt,b_filt, H_freq, w, H_eff] = freq_bw(N, 2.4e9, 20e6, 20e6, 9.9e6);
+%Correto 2.4e9
 %----------------------------------
-
 
 %-------------------------------------------%
 %              Modelagem CFO                %
@@ -34,8 +32,6 @@ Fator_Potencia=sqrt(Pot_N);
 %[cfo_hz] = cfo(Crystal_ppm, Fc)
 [cfo_hz] = cfo(10e-6, Fc);
 %----------------------------------
-
-
 
 %----------------------------------------------------%
 %            Declaraçao de vetores RX/TX             %
@@ -47,21 +43,24 @@ Fator_Potencia=sqrt(Pot_N);
 %-------------------------------------------%
 %            Lookup table (LUT)             %
 %-------------------------------------------%
-[p1, p2, p, combinacoes_LUT, LUT] = LUT(N,K,M);
-%-----------------------------------------------------------------------------
+[p1, p2, p_grupo, combinacoes_LUT, LUT_var] = LUT(n, k, M);
+p_total = G * p_grupo; 
+%correto
 
+%-----------------------------------------------------------------------------
 
 %-----------------------------------------------------------------------------%
 %           Catalogo de verossimilhança (Maximum Likelihood - ML)             %
 %-----------------------------------------------------------------------------%
-combinacoes=2^p;
-Catalogo_X = (zeros(N, combinacoes));
-Catalogo_Bits = zeros(combinacoes, p);
-for c=1:combinacoes
-[Catalogo_X, qam_sum, Catalogo_Bits, b_indice, b_sim] = ML(p, c, Catalogo_X, Catalogo_Bits, LUT, p1,M,K,Fator_Potencia,N);
+combinacoes = 2^p_grupo;
+Catalogo_X = zeros(n, combinacoes);
+Catalogo_Bits = zeros(combinacoes, p_grupo);
+for c = 1:combinacoes
+    [Catalogo_X, qam_sum, Catalogo_Bits, b_indice, b_sim] = ML(p_grupo, c, Catalogo_X, Catalogo_Bits, LUT_var, p1, M, k, Fator_Potencia, n);
 end
-%-----------------------------------------------------------------------------
+%correto
 
+%-----------------------------------------------------------------------------
 
 %-------------------------------------------%
 %            Transmissao                    %
@@ -77,52 +76,85 @@ bits_dac = 8;
 Delta_tx = 2 * max_tx_hardware / (2^bits_dac);
 Delta_rx = 2 * max_rx_hardware / (2^bits_adc);
  
+%painel de controle
+Habilita_CFO         = true;   
+Compensa_CFO         = true;
+Habilita_Quantizacao = true;
+Habilita_Filtro      = true;
+
+
+if ~Habilita_Filtro
+    H_eff = ones(N, 1);
+end
+
 for snr_indice = 1:length(EbNo_vec)
     
     EbNo_dB = EbNo_vec(snr_indice);
-    SNR_dB = EbNo_dB + 10*log10(p/(N+nCicP));
+    SNR_dB = EbNo_dB + 10*log10(p_total/(N+nCicP));
     
     erros = 0; 
     tempo = 0;
     txstate = zeros(max(length(a_filt)-1, length(b_filt)-1), 1);
     rxstate = zeros(max(length(a_filt)-1, length(b_filt)-1), 1);
     
-
 %--------------------------------------------------------%
 %               variaveis sensoriamento                  %
 %--------------------------------------------------------%
+%Resoluçao de range
+% 
+% \Delta R = \frac{c}{2B}, Sendo B a largura de banda do sinal. No caso
+% estudado, Delta R = 7.5
+    distancia=1;
+    velocidade=2;
 
-distancia=1;
-velocidade=1;
-
-Radar_tx_Matriz = zeros(N, num_blocos);
-Radar_rx_Matriz = zeros(N, num_blocos);
-
-
-
-
+    Radar_tx_Matriz = zeros(N, num_blocos);
+    Radar_rx_Matriz = zeros(N, num_blocos);
+    tempo_radar = 0;
 %----------------------------------------------%
+
+    
 
     for b = 1:num_blocos
         
-        [x_bloco, y_bloco, bits_tx, txstate, rxstate, tempo] = TxRx_Cascata(fs, Fator_Potencia, p, SNR_dB, LUT, N, nCicP, p1, M, K, a_filt, b_filt, txstate, rxstate, tempo, cfo_hz, Delta_tx, Delta_rx);
-        
-        [bits_rx, erros, x_total, y_total, y_eq] = eqlt(b, N, x_bloco, y_bloco, H_eff, Catalogo_X, Catalogo_Bits,bits_tx, erros, x_total, y_total, y_eq);
-            
-    
+        [x_bloco, y_bloco, bits_tx, txstate, rxstate, tempo] = TxRx_Cascata(fs, Fator_Potencia, p_grupo, p_total, SNR_dB, LUT_var, N, n, G, nCicP, p1, M, k, a_filt, b_filt, txstate, rxstate, tempo, cfo_hz, Delta_tx, Delta_rx, Habilita_CFO, Compensa_CFO, Habilita_Quantizacao, Habilita_Filtro);
 
+        [bits_rx, erros, x_total, y_total, y_eq] = eqlt(b, N, n, G, p_grupo, x_bloco, y_bloco, H_eff, Catalogo_X, Catalogo_Bits, bits_tx, erros, x_total, y_total, y_eq);
+        
+        
     %-----------------------------------------%
     %              Sensoriamento              %
     %-----------------------------------------%
     
-        fase_range=exp(-1i * 2 * pi * (0:N-1).' * distancia / N);
-        fase_doppler= exp(1i * 2 * pi * velocidade * b / num_blocos);
-    
-        y_radar_bloco = (x_bloco .* fase_range) * fase_doppler;
+        fase_range = exp(-1i * 2 * pi * (0:N-1).' * distancia / N);
+        fase_doppler = exp(1i * 2 * pi * velocidade * b / num_blocos);
+        alvo_refletido_freq = (x_bloco .* fase_range) * fase_doppler;
+        
+        alvo_tempo = ifft(alvo_refletido_freq, N);
+        
+        Vetor_tempo_radar = tempo_radar + (0:N-1).'/fs;
+        tempo_radar = Vetor_tempo_radar(end) + (1/fs);
+        
+        if Habilita_CFO && ~Compensa_CFO
+            Cfo_radar = exp(1i * 2 * pi * cfo_hz * Vetor_tempo_radar);
+            alvo_tempo = alvo_tempo .* Cfo_radar;
+        end
+
+        if Habilita_Filtro
+            alvo_tempo = filter(b_filt, a_filt, alvo_tempo);
+        end
+
+        if Habilita_Quantizacao
+            rx_re_radar = round(real(alvo_tempo)/Delta_rx)*Delta_rx;
+            rx_im_radar = round(imag(alvo_tempo)/Delta_rx)*Delta_rx;
+            alvo_tempo = rx_re_radar + (1i*rx_im_radar);
+        end
+        
+        y_radar_bloco = fft(alvo_tempo, N);
         y_radar_bloco = awgn(y_radar_bloco, SNR_dB, 'measured');
         
         Radar_tx_Matriz(:, b) = x_bloco;
         Radar_rx_Matriz(:, b) = y_radar_bloco;
+        
     %-----------------------------------------%
 
     end
@@ -135,7 +167,7 @@ Radar_rx_Matriz = zeros(N, num_blocos);
     %-----------------------------------------%
     %      Iniciando a calculo da BER         %
     %-----------------------------------------%
-    total_bits_TX = num_blocos * p;
+    total_bits_TX = num_blocos * p_total;
     BER(snr_indice) = erros / total_bits_TX;
     
     disp(['SNR = ', num2str(SNR_dB), ' dB | BER = ', num2str(BER(snr_indice))]);
@@ -159,7 +191,7 @@ plot(real(x_total), imag(x_total), 'ko', 'MarkerSize', 8, 'MarkerFaceColor', 'k'
 grid on;
 xlabel('Fase (I) - Parte Real');
 ylabel('Quadratura (Q) - Parte Imaginária');
-title(['Constelação OFDM-IM (N=', num2str(N), ', K=', num2str(K), ', ', num2str(M), '-QAM) - SNR = ', num2str(SNR_dB), ' dB']);
+title(['Constelação OFDM-IM (N=', num2str(N), ', K=', num2str(G*k), ', ', num2str(M), '-QAM) - SNR = ', num2str(SNR_dB), ' dB']);
 legend('Sinal Recebido (com ruído)', 'Sinal Transmitido (Ideal)', 'Location', 'best');
 
 % Fixa os limites dos eixos X e Y para -2 a +2 para visualização simétrica
@@ -178,7 +210,7 @@ semilogy(EbNo_vec, BER, 'bo-', 'LineWidth', 2, 'MarkerSize', 8, 'MarkerFaceColor
 grid on;
 xlabel('SNR (dB)');
 ylabel('Bit Error Rate (BER)');
-title(['Desempenho de BER do OFDM-IM (N=', num2str(N), ', K=', num2str(K), ', ', num2str(M), '-QAM)'])
+title(['Desempenho de BER do OFDM-IM (N=', num2str(N), ', K=', num2str(G*k), ', ', num2str(M), '-QAM)'])
 axis([min(EbNo_vec) max(EbNo_vec) 1e-5 1]); % Fixa os limites do gráfico
 png_BER = ['BER_ofdm_im_HardwareLimitado_', timestamp, '.png'];
 exportgraphics(fig_BER, png_BER, 'Resolution', 300);
@@ -187,14 +219,17 @@ exportgraphics(fig_BER, png_BER, 'Resolution', 300);
 
 % 5. Mapa range doppler
 fig_RangeDoppler=figure;
-doppler_axis = linspace(0, 10, num_blocos);
+doppler_axis = linspace(-num_blocos/2, num_blocos/2 - 1, num_blocos);
 range_axis = 0:(N-1);
 
-mesh(doppler_axis, range_axis, 10*log10(abs(Range).^2));
+mesh(doppler_axis, range_axis, 10*log10(abs(Doppler).^2));
 view(2);
 xlabel('Frequência Doppler (Velocidade)');
 ylabel('Range (Distância)');
 title(['Mapa Range-Doppler OFDM-IM | SNR = ', num2str(SNR_dB), ' dB']);
 colorbar;
+
+axis([-30 40 0 30]); 
+
 png_RD = ['RangeDoppler_ofdm_im_HardwareLimitado_', timestamp, '.png'];
 exportgraphics(fig_RangeDoppler, png_RD, 'Resolution', 300);
--- TxRx_Cascata.m
+++ TxRx_Cascata.m
@@ -2,21 +2,28 @@
 %            Transmissao                    %
 %-------------------------------------------%
 
-function [x_bloco, y_bloco, bits_tx, txstate, rxstate, tempo] = TxRx_Cascata(fs, Fator_Potencia, p, SNR_dB, LUT, N, nCicP, p1, M, K, a_filt, b_filt, txstate, rxstate, tempo, cfo_hz, Delta_tx, Delta_rx)
+function [x_bloco, y_bloco, bits_tx, txstate, rxstate, tempo] = TxRx_Cascata(fs, Fator_Potencia, p_grupo, p_total, SNR_dB, LUT, N, n, G, nCicP, p1, M, k, a_filt, b_filt, txstate, rxstate, tempo, cfo_hz, Delta_tx, Delta_rx, Habilita_CFO, Compensa_CFO, Habilita_Quantizacao, Habilita_Filtro)
 
-    bits_tx = randi([0 1], 1, p);
-    bits_indice  = bits_tx(1:p1);
-    bits_simbolo = bits_tx(p1+1:end); 
-
-    dec_indice = bit2int(bits_indice.', p1);
-    indices_ativos = LUT(dec_indice + 1, :); % +1 pois o MATLAB começa no índice 1
-
-    b_sim_matriz = reshape(bits_simbolo, log2(M), K);
-    simbolos_dec = bit2int(b_sim_matriz, log2(M));
-    simbolos_qam = qammod(simbolos_dec, M, 'UnitAveragePower', true) * Fator_Potencia;
-
+    bits_tx = randi([0 1], 1, p_total);
     x_bloco = zeros(N, 1);
-    x_bloco(indices_ativos) = simbolos_qam; 
+    
+    for g = 1:G
+        offset_bit = (g-1) * p_grupo;
+        bits_grupo = bits_tx(offset_bit+1 : offset_bit+p_grupo);
+        
+        bits_indice  = bits_grupo(1:p1);
+        bits_simbolo = bits_grupo(p1+1:end); 
+
+        dec_indice = bit2int(bits_indice.', p1);
+        indices_ativos = LUT(dec_indice + 1, :); % +1 pois o MATLAB começa no índice 1
+
+        b_sim_matriz = reshape(bits_simbolo, log2(M), k);
+        simbolos_dec = bit2int(b_sim_matriz, log2(M));
+        simbolos_qam = qammod(simbolos_dec, M, 'UnitAveragePower', true) * Fator_Potencia;
+
+        offset_idx = (g-1) * n;
+        x_bloco(indices_ativos + offset_idx) = simbolos_qam; 
+    end
 
     x_tempo = ifft(x_bloco, N);
     %inserçao do prefixo ciclico
@@ -25,22 +32,38 @@ function [x_bloco, y_bloco, bits_tx, txstate, rxstate, tempo] = TxRx_Cascata(fs,
     %-------------------------------------------%
     %              Imperfeiçoes                 %
     %-------------------------------------------%
-    tx_re = round(real(x_Cic)/Delta_tx)*Delta_tx;
-    tx_im = round(imag(x_Cic)/Delta_tx)*Delta_tx;
-    x_Cic_Quantizado = tx_re + (1i* tx_im);
-    
-    [x_Cic_Filtrado, txstate] = filter(b_filt, a_filt, x_Cic_Quantizado, txstate);
+    if Habilita_Quantizacao
+        max_tx = (2^11) * Delta_tx; 
+        re_limitado = max(min(real(x_Cic), max_tx), -max_tx);
+        im_limitado = max(min(imag(x_Cic), max_tx), -max_tx);
+        
+        tx_re = round(re_limitado/Delta_tx)*Delta_tx;
+        tx_im = round(im_limitado/Delta_tx)*Delta_tx;
+        x_Cic_Quantizado = tx_re + (1i* tx_im);
+    else
+        x_Cic_Quantizado = x_Cic;
+    end
 
+    if Habilita_Filtro
+        [x_Cic_Filtrado, txstate] = filter(b_filt, a_filt, x_Cic_Quantizado, txstate);
+    else
+        x_Cic_Filtrado = x_Cic_Quantizado;
+    end
+    
     % Aplicando o desvio de frequencia no canal quantizado e filtrado (CFO)
     Vetor_tempo = tempo + (0:length(x_Cic_Filtrado)-1).'/fs; 
-    tempo = Vetor_tempo(end) + (1/fs); 
-    
-    Cfo = exp(1i*2*pi*cfo_hz*Vetor_tempo);
-    x_Cic_Final = x_Cic_Filtrado .* Cfo;
+    tempo = Vetor_tempo(end) + (1/fs);
 
+   if Habilita_CFO
+        Cfo = exp(1i*2*pi*cfo_hz*Vetor_tempo);
+        x_Cic_Final = x_Cic_Filtrado .* Cfo;
+    else
+        x_Cic_Final = x_Cic_Filtrado;
+    end
+    
     %Desse modo, o ruido atua sobre o sinal estendido pelo prefixo e quantizado
     y_Cic = awgn(x_Cic_Final, SNR_dB, 'measured');
 
-    [rxstate, y_bloco, ~] = reconstrutor(cfo_hz, N, Vetor_tempo, y_Cic, a_filt, b_filt, Delta_rx, nCicP, rxstate);
-
+    % Atualize a chamada do reconstrutor passando as duas flags de CFO separadas
+    [rxstate, y_bloco, ~] = reconstrutor(cfo_hz, N, Vetor_tempo, y_Cic, a_filt, b_filt, Delta_rx, nCicP, rxstate, Compensa_CFO, Habilita_Quantizacao, Habilita_Filtro);
 end
